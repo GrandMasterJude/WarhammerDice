f5ecca7dbb122ae51a4571482da1fae928a54181
Jude McCulley <GrandMasterJude@gmail.com>
2019-06-02 10:03:27 -0500

    Made some style changes

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ function DoMath() {
     let hitWoundWSave = (reduce(hitFraction[0]*woundFraction[0]*wSaveFraction[0],hitFraction[1]*woundFraction[1]*wSaveFraction[1]));
     let hitWoundASaveWSave= (reduce(hitFraction[0]*woundFraction[0]*aSaveFraction[0]*wSaveFraction[0],hitFraction[1]*woundFraction[1]*aSaveFraction[1]*wSaveFraction[1]));
 
-    
+   
+
     // This should probably have an hourglass shaped structure of checking all the dropdowns
     // Then doing the math and stuff, then checking as it prints each item
     // Not a structure of check what all is there first and then do a different set of code for each grouping
@@ -42,8 +43,8 @@ function DoMath() {
     }
     else if (toASave.value == "None") {
         total = hitChance * woundChance * wSaveChance;
-        wSaveFractionP.innerHTML = (" " + wSaveFraction[0] + " / " + wSaveFraction[1] + " So far we have " +
-        hitWoundWSave[0] + " / " + hitWoundWSave[1]);
+        wSaveFractionP.innerHTML = (" " + wSaveFraction[0] + " / " + wSaveFraction[1]);
+        wSaveFractionP2.innerHTML = (" " + " So far we have " + hitWoundWSave[0] + " / " + hitWoundWSave[1]);
         totalFractionP.innerHTML = (" " + hitWoundWSave[0] + " / " + hitWoundWSave[1]);
 
     }
@@ -56,8 +57,8 @@ function DoMath() {
     }
     else {
         total = hitChance * woundChance * aSaveChance * wSaveChance;
-        aSaveFractionP.innerHTML = (" " + aSaveFraction[0] + " / " + aSaveFraction[1] + " So far we have " +
-        hitWoundASave[0] + " / " + hitWoundASave[1]);
+        aSaveFractionP.innerHTML = (" " + aSaveFraction[0] + " / " + aSaveFraction[1] + "<p> So far we have " +
+        hitWoundASave[0] + " / " + hitWoundASave[1]) + "</p>";
         wSaveFractionP.innerHTML = (" " + wSaveFraction[0] + " / " + wSaveFraction[1] + " So far we have " +
         hitWoundWSave[0] + " / " + hitWoundWSave[1]);
         totalFractionP.innerHTML = (" " + hitWoundASaveWSave[0] + " / " + hitWoundASaveWSave[1]);
@@ -67,8 +68,8 @@ function DoMath() {
 
 
     hitFractionP.innerHTML = (" " + hitFraction[0] + " / " + hitFraction[1]);
-    woundFractionP.innerHTML = (" " + woundFraction[0] + " / " + woundFraction[1] + " So far we have " +
-    hitWound[0] + " / " + hitWound[1]);
+    woundFractionP.innerHTML = (" " + woundFraction[0] + " / " + woundFraction[1]);
+    woundFractionP2.innerHTML = (" " + " So far we have " + hitWound[0] + " / " + hitWound[1]);
 
 
 }
@@ -89,7 +90,7 @@ function ToHitCalc(hits) {
         console.log("missedHits : " + missedHits);
         console.log("rehits : " + rehits);
         console.log("Total Hits : " + hitTotal);
-        
+        console.log(mul(hitHits,missedHits));
     } 
 
     
@@ -102,6 +103,14 @@ function ToHitCalc(hits) {
 
 // refactor changing each different innerhtml to be it's own function
 
+function mul(a, b) {
+    parse(a, b);
+    return new Fraction(
+            this["s"] * P["s"] * this["n"] * P["n"],
+            this["d"] * P["d"]
+            );
+  }
+
 
 // Reduce a fraction by finding the Greatest Common Divisor and dividing by it.
 function reduce(numerator,denominator){
@@ -113,3 +122,188 @@ function reduce(numerator,denominator){
   }
 
 DoMath();
+
+var P = {
+    "s": 1,
+    "n": 0,
+    "d": 1
+  };
+
+var parse = function(p1, p2) {
+
+    var n = 0, d = 1, s = 1;
+    var v = 0, w = 0, x = 0, y = 1, z = 1;
+
+    var A = 0, B = 1;
+    var C = 1, D = 1;
+
+    var N = 10000000;
+    var M;
+
+    if (p1 === undefined || p1 === null) {
+      /* void */
+    } else if (p2 !== undefined) {
+      n = p1;
+      d = p2;
+      s = n * d;
+    } else
+      switch (typeof p1) {
+
+        case "object":
+        {
+          if ("d" in p1 && "n" in p1) {
+            n = p1["n"];
+            d = p1["d"];
+            if ("s" in p1)
+              n *= p1["s"];
+          } else if (0 in p1) {
+            n = p1[0];
+            if (1 in p1)
+              d = p1[1];
+          } else {
+            throwInvalidParam();
+          }
+          s = n * d;
+          break;
+        }
+        case "number":
+        {
+          if (p1 < 0) {
+            s = p1;
+            p1 = -p1;
+          }
+
+          if (p1 % 1 === 0) {
+            n = p1;
+          } else if (p1 > 0) { // check for != 0, scale would become NaN (log(0)), which converges really slow
+
+            if (p1 >= 1) {
+              z = Math.pow(10, Math.floor(1 + Math.log(p1) / Math.LN10));
+              p1 /= z;
+            }
+
+            // Using Farey Sequences
+            // http://www.johndcook.com/blog/2010/10/20/best-rational-approximation/
+
+            while (B <= N && D <= N) {
+              M = (A + C) / (B + D);
+
+              if (p1 === M) {
+                if (B + D <= N) {
+                  n = A + C;
+                  d = B + D;
+                } else if (D > B) {
+                  n = C;
+                  d = D;
+                } else {
+                  n = A;
+                  d = B;
+                }
+                break;
+
+              } else {
+
+                if (p1 > M) {
+                  A += C;
+                  B += D;
+                } else {
+                  C += A;
+                  D += B;
+                }
+
+                if (B > N) {
+                  n = C;
+                  d = D;
+                } else {
+                  n = A;
+                  d = B;
+                }
+              }
+            }
+            n *= z;
+          } else if (isNaN(p1) || isNaN(p2)) {
+            d = n = NaN;
+          }
+          break;
+        }
+        case "string":
+        {
+          B = p1.match(/\d+|./g);
+
+          if (B === null)
+            throwInvalidParam();
+
+          if (B[A] === '-') {// Check for minus sign at the beginning
+            s = -1;
+            A++;
+          } else if (B[A] === '+') {// Check for plus sign at the beginning
+            A++;
+          }
+
+          if (B.length === A + 1) { // Check if it's just a simple number "1234"
+            w = assign(B[A++], s);
+          } else if (B[A + 1] === '.' || B[A] === '.') { // Check if it's a decimal number
+
+            if (B[A] !== '.') { // Handle 0.5 and .5
+              v = assign(B[A++], s);
+            }
+            A++;
+
+            // Check for decimal places
+            if (A + 1 === B.length || B[A + 1] === '(' && B[A + 3] === ')' || B[A + 1] === "'" && B[A + 3] === "'") {
+              w = assign(B[A], s);
+              y = Math.pow(10, B[A].length);
+              A++;
+            }
+
+            // Check for repeating places
+            if (B[A] === '(' && B[A + 2] === ')' || B[A] === "'" && B[A + 2] === "'") {
+              x = assign(B[A + 1], s);
+              z = Math.pow(10, B[A + 1].length) - 1;
+              A += 3;
+            }
+
+          } else if (B[A + 1] === '/' || B[A + 1] === ':') { // Check for a simple fraction "123/456" or "123:456"
+            w = assign(B[A], s);
+            y = assign(B[A + 2], 1);
+            A += 3;
+          } else if (B[A + 3] === '/' && B[A + 1] === ' ') { // Check for a complex fraction "123 1/2"
+            v = assign(B[A], s);
+            w = assign(B[A + 2], s);
+            y = assign(B[A + 4], 1);
+            A += 5;
+          }
+
+          if (B.length <= A) { // Check for more tokens on the stack
+            d = y * z;
+            s = /* void */
+                    n = x + d * v + z * w;
+            break;
+          }
+
+          /* Fall through on error */
+        }
+        default:
+          throwInvalidParam();
+      }
+
+    if (d === 0) {
+      throw new DivisionByZero();
+    }
+
+    P["s"] = s < 0 ? -1 : 1;
+    P["n"] = Math.abs(n);
+    P["d"] = Math.abs(d);
+  };
+
+  function modpow(b, e, m) {
+
+    var r = 1;
+    for (; e > 0; b = (b * b) % m, e >>= 1) {
+
+      if (e & 1) {
+        r = (r * b) % m;
+      }
+    }
+    return r;
+  }
